4fd7b6051e340a754ec804156d2c43c1c91cd3c8
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-20 23:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    for (let char of str) {
+        if (str.indexOf(char) === str.lastIndexOf(char)) {
+            return char;
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
